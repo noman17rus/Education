239b8139fd71a512e97e3f0e8d50ad346555ce31
noman17rus <noman171991@gmail.com>
2020-12-01 18:07:57 +0400

    init3

--- src/Initialization/InitialValues.java
+++ src/Initialization/InitialValues.java
@@ -14,4 +14,27 @@ public class InitialValues {
     float f;
     double d;
     InitialValues reference;
+    void printInitValues(){
+        System.out.println("Тип данных               начальное значение ");
+        System.out.println("bollean "                + t );
+        System.out.println("char                     [" + c + "]");
+        System.out.println("short                    " + s);
+        System.out.println("int                      " + i);
+        System.out.println("long                     " + l);
+        System.out.println("float                    " + f);
+        System.out.println("dounle                   " + d);
+        System.out.println("reference                " + reference);
+    }
+
+    public static void main(String[] args) {
+        InitialValues iv = new InitialValues();
+        iv.printInitValues();
+        //вызывается метод вывода в консоль инизиализированных
+        // ПРИМИТИВНЫЕ ТИПЫ ИНИЦИАЛИЗИРУЮТСЯ ПО УМОЛЧАНИЮ т.е в консоли они должны иметь значения
+        //пример: int = 0
+        /**
+         * возможет второй вариант вызова метода
+         */
+        new InitialValues().printInitValues();
+    }
 }
